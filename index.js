11d7ff4af333bfc53b8ca2ebd99ef94c6235dd00
Evan Pavley <epavley@sva.edu>
2018-11-26 22:28:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 function takeANumber (katzDeliLine, name){
+  katzDeliLine.push(name);
+  var place = katzDeliLine.length;
   
-  var place = katzDeliLine.length+1
-  katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + place + " in line."
-
+  return "Welcome, " + name + ". You are number " + place + " in line.";
 }
 
 function nowServing (katzDeliLine){
  if (katzDeliLine.length > 0){
-   var report = `Currently serving ${katzDeliLine[0]}.`;
-   katzDeliLine.shift();
-   return report;
+   return `Currently serving ${katzDeliLine.shift()}.`;
+   
  } else {
-   return "There is nobody waiting to be served!"
+   return "There is nobody waiting to be served!";
  }
 }
 
-function currentLine(katzDeli){
-  var line = [];
-  if (katzDeli.length === 0){
+function currentLine(katzDeliLine){
+  var number = [];
+  
+  if (katzDeliLine.length === 0){
     return "The line is currently empty.";
-  }
-  else{
-    for (let i = 0; i < katzDeli.length; i++){
-      line.push(' '+[i+1]+'. ' + katzDeli[i])
+  
+  } else{
+    for (let i = 0; i < katzDeliLine.length; i++){
+      number.push(`${i + 1}. ${katzDeliLine[i]}`);
     }
-    return('The line is currently:' + line)
+    
+    return `The line is currently: ${number.join(', ')}`;
   }
 }
